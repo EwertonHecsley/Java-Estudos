f7478b20c3a87b2100b7ad885798cf70ff08403c
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-09-16 14:02:49 -0300

    Subindo cod

--- tipos-variaveis/src/TiposVariaves.java
+++ tipos-variaveis/src/TiposVariaves.java
@@ -1,5 +1,10 @@
 public class TiposVariaves {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final int ANO_NASCIMENTO = 1989;
+        int idade = 33;
+        String nome = "Ewerton Hecsley";
+        double altura = 1.75;
+
+        System.out.println(ANO_NASCIMENTO);
     }
 }
